733f47cd92f9c58bfa4b320b249028bf3140146e
dimitrisarrazinkapeu <dimitri.sarrazin.pro.dev@gmail.com>
2021-11-16 14:13:10 +0100

    Header v1

--- index.php
+++ index.php
@@ -8,4 +8,20 @@ echo 'La page Dev avant de envoyer en master';
 echo 'mettre juste les trucs qui marchent :D';
 
 
-include('inc/header.php');
+include('inc/header.php'); ?>
+
+<header id="container_header">
+        <div class="logo_vactolib">
+            <h1><img src="asset/img/unknown.png"></h1>
+        </div>
+        <div class="items_header">
+            <nav>
+                <ul class="nav_header">
+                    <li><a href="#">vie privée</a></li>
+                    <li><a href="#">inscription</a></li>
+                    <li><a href="#">connexion</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
