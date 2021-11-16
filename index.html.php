5c81312b7664a676d4ecd595b1f1ae5529f10811
fradintheo <theofradin@outlook.com>
2021-11-16 13:57:37 +0100

    ajout branch dev

--- index.html.php
+++ index.html.php
@@ -3,7 +3,8 @@ require('inc/fonction.php');
 require('inc/request.php');
 
 
-
+echo 'La page Dev avant de envoyer en master';
+echo 'mettre juste les trucs qui marchent :D';
 
 
 include('inc/header.php'); ?>
