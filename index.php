5a75fa677bb17307e82aaf324cb8f3715f85fd2b
fradintheo <theofradin@outlook.com>
2021-11-16 14:28:43 +0100

    ajout image

--- index.php
+++ index.php
@@ -10,18 +10,3 @@ echo 'mettre juste les trucs qui marchent :D';
 
 include('inc/header.php'); ?>
 
-<header id="container_header">
-        <div class="logo_vactolib">
-            <h1><img src="asset/img/unknown.png"></h1>
-        </div>
-        <div class="items_header">
-            <nav>
-                <ul class="nav_header">
-                    <li><a href="#">vie privée</a></li>
-                    <li><a href="#">inscription</a></li>
-                    <li><a href="#">connexion</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</header>
