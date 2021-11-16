5f281035d71106ac0629e30b44200baaff03df1c
dimitrisarrazinkapeu <dimitri.sarrazin.pro.dev@gmail.com>
2021-11-16 14:46:16 +0100

    v1.1

--- index.php
+++ index.php
@@ -11,12 +11,17 @@ echo 'mettre juste les trucs qui marchent :D';
 include('inc/header.php'); ?>
 
 <section>
-    <p>Vactolib est une application
-        pilotée par l'État pour vous retrouver dans vos différents vaccins.</p>
-    <a href="#">Inscription</a>
-    <a href="#">Connexion</a>
-    <div class="items_groupe">
-        <img src=""
+    <div class="container">
+        <div class="wrap">
+            <div class="items">
+                <p>Vactolib est une application
+                    pilotée par l'État pour vous retrouver dans vos différents vaccins.</p>
+                <a href="#">Inscription</a>
+                <a href="#">Connexion</a>
+                <div class="items_groupe">
+                    <img src="asset/img/groupe.png">
+                </div>
+            </div>
+        </div>
     </div>
-
 </section>
