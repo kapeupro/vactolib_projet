ef5d14165a1388ecae99d265b12af5e1684a6599
Pintojody <pintojody20@gmail.com>
2021-11-16 16:40:58 +0100

    footer html terminé + debut css

--- inc/header.php
+++ inc/header.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Arvo:ital@1&family=Nunito:ital,wght@0,400;0,700;1,300&display=swap" rel="stylesheet">
     <title>Vactolib, votre carnet de santé en ligne</title>
     <link rel="stylesheet" href="asset/css/style.css">
