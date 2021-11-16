a3c36e1bfd51a6d3c6449c8fe0a3ffe69cd158c4
dimitrisarrazinkapeu <dimitri.sarrazin.pro.dev@gmail.com>
2021-11-16 15:40:27 +0100

    ajout de la font

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arvo:ital@1&family=Nunito:ital,wght@0,400;0,700;1,300&display=swap" rel="stylesheet">
     <title>Vactolib, votre carnet de santé en ligne</title>
     <link rel="stylesheet" href="asset/css/style.css">
 </head>
