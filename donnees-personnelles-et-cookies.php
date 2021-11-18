e1cf9e17d7795b5da93906afa3364a4a21622132
dimitrisarrazinkapeu <dimitri.sarrazin.pro.dev@gmail.com>
2021-11-18 10:48:51 +0100

    css mentionslegal.php

--- donnees-personnelles-et-cookies.php
+++ donnees-personnelles-et-cookies.php
@@ -8,7 +8,7 @@ include('inc/header.php'); ?>
 
 <section>
     <div class="wrap">
-    <div class="title-h2-donnees-personnelles-et-cookies">
+    <div id="title-h2-donnees-personnelles-et-cookies">
         <h2>Données personnelles et cookies</h2>
     </div>
     <div class="items-1-donnees-personnelles-et-cookies">
