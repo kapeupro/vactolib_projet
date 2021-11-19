67c9bcb045e8d4d83b4d12324fec9325b45a44a0
dimitrisarrazinkapeu <dimitri.sarrazin.pro.dev@gmail.com>
2021-11-19 12:03:16 +0100

    vieprivee

--- vieprivee.php
+++ vieprivee.php
@@ -9,31 +9,63 @@ require('inc/request.php');
 
 include('inc/header.php'); ?>
 
-<section>
-    <div class="title_vieprivee">
-        <h2>Avec Vactolib, vous êtes les chefs de vos données !</h2>
-    </div>
-    <div class="wrap">
-        <div class="container_vieprivee_1">
-            <div class="items_vie_1">
-                <h3>Les crypto-identifiants échangés sont anonymes et sécurisés</h3>
-                <p>Les données de TousAntiCovid sont stockées localement sur votre téléphone et peuvent être supprimées à tout moment.
-                    Son fonctionnement rend impossible, et pour quiconque, la possiblité de reconstituer la liste des personnes ayant
-                    déclaré être contaminées. Il s'agit du principe au cœur de l’application, de telle manière que personne ne puisse ni
-                    retracer la liste des personnes testées positives ni, le cas échéant, reconstituer la chaîne de transmission.</p>
-            </div>
-            <div class="image_vieprivee_1"></div>
+    <section>
+        <div class="title_vieprivee">
+            <h2>Avec Vactolib, vous êtes les chefs de vos données !</h2>
         </div>
-        <div class="container_vieprivee_2">
-            <div class="items_vie_2">
-                <h3>Les crypto-identifiants échangés sont anonymes et sécurisés</h3>
-                <p>Les données de TousAntiCovid sont stockées localement sur votre téléphone et peuvent être supprimées à tout moment.
-                    Son fonctionnement rend impossible, et pour quiconque, la possiblité de reconstituer la liste des personnes ayant
-                    déclaré être contaminées. Il s'agit du principe au cœur de l’application, de telle manière que personne ne puisse ni
-                    retracer la liste des personnes testées positives ni, le cas échéant, reconstituer la chaîne de transmission.</p>
+        <div class="wrap">
+            <div class="container_vieprivee_1">
+                <div class="items_vie_1">
+                    <h3>Les crypto-identifiants échangés sont anonymes et sécurisés</h3>
+                    <p>Les données de TousAntiCovid sont stockées localement sur votre téléphone et peuvent être supprimées à tout moment.
+                        Son fonctionnement rend impossible, et pour quiconque, la possiblité de reconstituer la liste des personnes ayant
+                        déclaré être contaminées. Il s'agit du principe au cœur de l’application, de telle manière que personne ne puisse ni
+                        retracer la liste des personnes testées positives ni, le cas échéant, reconstituer la chaîne de transmission.</p>
+                </div>
+                <div class="image_vieprivee_1"></div>
             </div>
-            <div class="image_vieprivee_1"></div>
-    </div>
-</section>
+
+            <div class="container_vieprivee_2">
+                <div class="items_vie_2">
+                    <h3>Les crypto-identifiants échangés sont anonymes et sécurisés</h3>
+                    <p>Les données de TousAntiCovid sont stockées localement sur votre téléphone et peuvent être supprimées à tout moment.
+                        Son fonctionnement rend impossible, et pour quiconque, la possiblité de reconstituer la liste des personnes ayant
+                        déclaré être contaminées. Il s'agit du principe au cœur de l’application, de telle manière que personne ne puisse ni
+                        retracer la liste des personnes testées positives ni, le cas échéant, reconstituer la chaîne de transmission.</p>
+                </div>
+                <div class="image_vieprivee_1"></div>
+            </div>
+                <div class="container_vieprivee_1">
+                    <div class="items_vie_1">
+                        <h3>Les crypto-identifiants échangés sont anonymes et sécurisés</h3>
+                        <p>Les données de TousAntiCovid sont stockées localement sur votre téléphone et peuvent être supprimées à tout moment.
+                            Son fonctionnement rend impossible, et pour quiconque, la possiblité de reconstituer la liste des personnes ayant
+                            déclaré être contaminées. Il s'agit du principe au cœur de l’application, de telle manière que personne ne puisse ni
+                            retracer la liste des personnes testées positives ni, le cas échéant, reconstituer la chaîne de transmission.</p>
+                    </div>
+                    <div class="image_vieprivee_1"></div>
+                </div>
+                    <div class="container_vieprivee_1">
+                        <div class="items_vie_1">
+                            <h3>Les crypto-identifiants échangés sont anonymes et sécurisés</h3>
+                            <p>Les données de TousAntiCovid sont stockées localement sur votre téléphone et peuvent être supprimées à tout moment.
+                                Son fonctionnement rend impossible, et pour quiconque, la possiblité de reconstituer la liste des personnes ayant
+                                déclaré être contaminées. Il s'agit du principe au cœur de l’application, de telle manière que personne ne puisse ni
+                                retracer la liste des personnes testées positives ni, le cas échéant, reconstituer la chaîne de transmission.</p>
+                        </div>
+                        <div class="image_vieprivee_1"></div>
+                    </div>
+                        <div class="container_vieprivee_1">
+                            <div class="items_vie_1">
+                                <h3>Les crypto-identifiants échangés sont anonymes et sécurisés</h3>
+                                <p>Les données de TousAntiCovid sont stockées localement sur votre téléphone et peuvent être supprimées à tout moment.
+                                    Son fonctionnement rend impossible, et pour quiconque, la possiblité de reconstituer la liste des personnes ayant
+                                    déclaré être contaminées. Il s'agit du principe au cœur de l’application, de telle manière que personne ne puisse ni
+                                    retracer la liste des personnes testées positives ni, le cas échéant, reconstituer la chaîne de transmission.</p>
+                            </div>
+                            <div class="image_vieprivee_1"></div>
+                        </div>
+                        </div>
+    </section>
 
 <?php include('inc/footer.php');
