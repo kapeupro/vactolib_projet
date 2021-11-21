b6218e2acc394f7bcd17df46f9146674cfa7d09b
dimitrisarrazinkapeu <dimitri.sarrazin.pro.dev@gmail.com>
2021-11-21 22:46:42 +0100

    test correction partir anthony..

--- inc/pdo.php
+++ inc/pdo.php
@@ -1,7 +1,10 @@
 <?php
-
+$serveur  = '%';
+$bdd      = 'wtgm7011_vactolib';
+$username = 'wtgm7011_admin';
+$password = 'Azerty1999';
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=vactolib', "root", "", array(
+    $pdo = new PDO('mysql:host='.$serveur.';dbname='.$bdd.'', ''.$username.'', ''.$password.'', array(
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
