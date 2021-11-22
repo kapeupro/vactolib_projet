56f5c0a9be2a9fa48b2d07d4be2e5ef3620191b2
Pintojody <pintojody20@gmail.com>
2021-11-22 14:52:13 +0100

    modif lien admin

--- admin/admin-test/template/index.php
+++ admin/admin-test/template/index.php
@@ -68,7 +68,7 @@ $allAjout= $query->fetch();
         <nav class="sidebar sidebar-offcanvas" id="sidebar">
             <ul class="nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">
+                    <a class="nav-link" href="index.php">
                         <i class="icon-grid menu-icon"></i>
                         <span class="menu-title">Dashboard</span>
                     </a>
