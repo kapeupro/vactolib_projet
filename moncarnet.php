a8497b305f6899cc2673dde34811804f8cc80aba
fradintheo <theofradin@outlook.com>
2021-11-25 21:10:08 +0100

    maj theo

--- moncarnet.php
+++ moncarnet.php
@@ -47,7 +47,7 @@ include('inc/header.php'); ?>
                             <div class="items-carnet">
                                 <h3>Vaccination <?php echo $user_vaccins[$i]['nom_vaccin']; ?></h3>
                                 <p> <?php echo $user['nom'];echo' ';echo $user['prenom'] ?></p>
-<!--                                <p> Rappel le --><?php //echo date_add($user_vaccins[$i]['vaccin_date'],date_interval_create_from_date_string($user_vaccins[$i]['vaccin_rappel'])) ?><!-- </p>-->
+<!--                                <p> Rappel le --><?php //echo date_add($user_vaccins[$i]['vaccin_date'],date_interval_create_from_date_string($user_vaccins[$i]['vaccin_rappel'])) ?><!-- </p>-->git p
                                 <p><?php echo $user_vaccins[$i]['laboratoire'] ?> fait le <?php echo dateFormatWithoutHour($user_vaccins[$i]['vaccin_date'], 'd/m/Y') ?></p>
                                 <a class="button_type1" href="detail.php?id=<?php echo $user_vaccins[$i]['id']; ?> "> Plus d'info </a>
                                 <a onclick="return confirm('Voulez-vous vraiment supprimer ce vaccin de votre carnet ?')" class="button_type2" href="delete.php?id=<?php echo $user_vaccins[$i]['id'] ?>"> Supprimer </a>
